954220b4640895a470f82d7ebe8dded05ad7602a
HuseynGPT <huseynovbabek1@gmail.com>
2024-10-06 11:08:55 +0400

    Add project files.

---
+++ SocialNetwork.Entities/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace SocialNetwork.Entities.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    [Required]
+    public string? FirstName { get; set; }
+    [Required]
+    public string? LastName { get; set; }
+    [Required]
+    public string? Email { get; set; }
+    [Required]
+    public string? Password { get; set; }   
+    public string? Gender { get; set; }
+    public string? Phone { get; set; }
+    public bool HasOnline { get; set; }
+    public string? ImageUrl { get; set; }
+}
---
+++ SocialNetwork.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.WebUI.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
