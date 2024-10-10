9aa7919006e7f21b5c9dd3b2da92c8d5c2459910
HuseynGPT <huseynovbabek1@gmail.com>
2024-10-10 07:37:25 +0400

    zv

--- SocialNetwork.WebUI/Controllers/AccountController.cs
+++ SocialNetwork.WebUI/Controllers/AccountController.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Business.Services.Abstracts;
+using SocialNetwork.Entities.Entities;
 
 namespace SocialNetwork.WebUI.Controllers;
 
 public class AccountController : Controller
 {
-    public IActionResult Index()
+  private readonly UserManager<CustomIdentityUser> _userManager;
+    private readonly RoleManager<CustomIdentityRole> _roleManager;
+    private readonly SignInManager<CustomIdentityUser> _signInManager;
+    private ICustomIdentityUserService _customIdentityUserService;
+
+    public AccountController(UserManager<CustomIdentityUser> userManager, RoleManager<CustomIdentityRole> roleManager, SignInManager<CustomIdentityUser> signInManager, ICustomIdentityUserService customIdentityUserService)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _signInManager = signInManager;
+        _customIdentityUserService = customIdentityUserService;
+    }
+    [HttpGet]
+    public IActionResult Login()
     {
         return View();
     }
---
+++ SocialNetwork.WebUI/Models/ChatViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SocialNetwork.Entities.Entities;
+
+namespace SocialNetwork.WebUI.Models
+{
+    public class ChatViewModel
+    {
+        public string? CurrentUserId { get; set; }
+        public Chat? CurrentChat { get; set; }
+        public IEnumerable<Chat>? Chats { get; set; }
+        public string? CurrentReceiver { get; internal set; }
+        public string? CurrentUserName {get;set; }
+        public string? CurrentReceiverImage { get;  set; }
+    }
+}
